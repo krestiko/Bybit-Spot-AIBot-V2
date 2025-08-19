15470c1fad2546a5ca3f4a66a9aa92c298faf322
krestiko <krestiko.worker@gmail.com>
2025-08-19 13:21:54 +0300

    Update test_trade.py

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -1,10 +1,21 @@
+mport os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from bybitbot import TradingBot
 
 
 def test_compute_trade_amount_basic():
     bot = TradingBot()
     for i in range(12):
-        bot.append_market_data(price=1 + i, volume=1, bid=1, ask=1)
+        bot.append_market_data(
+            price=1 + i,
+            high=1 + i + 0.5,
+            low=1 + i - 0.5,
+            volume=1,
+            bid=1,
+            ask=1,
+        )
     amt = bot.compute_trade_amount()
     assert amt <= bot.trade_amount
     assert amt > 0
